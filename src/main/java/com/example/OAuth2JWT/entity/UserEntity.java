e9528cd65d5df3ad191ad4393b46044a3629c714
janggoni <wkdgudrhs@naver.com>
2025-02-10 17:28:38 +0900

    first commit

---
+++ src/main/java/com/example/OAuth2JWT/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package com.example.OAuth2JWT.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String name;
+
+    private String email;
+
+    private String role;
+}
